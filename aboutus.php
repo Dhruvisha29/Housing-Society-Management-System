f7ab86c2fea67b0400cf5f213f5002fe82a521e4
Dhruvisha <dhruvishavasoya29@gmail.com>
2021-10-03 10:02:30 +0530

    About us page

---
+++ aboutus.php
@@ -0,0 +1,27 @@
+<?php $page= 'aboutus'; include 'template/header.php'; ?>
+<!--==========================
+      About Section
+    ============================-->
+    <section id="about" class="wow fadeInUp">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 about-img">
+            <img src="img/DI2_6501684.jpg" alt="">
+          </div>
+
+          <div class="col-lg-6 content">
+            <h2>We have the best maintenance system for you...</h2>
+            <h3>SMS is helping both the residents and managing commitee interacts seemlessly with each other.</h3>
+
+            <ul>
+              <li><i class="ion-android-checkmark-circle"></i> Raise maintenace demand notes every month.</li>
+              <li><i class="ion-android-checkmark-circle"></i> Our main aim is to try to fullfill the needs of the customer.</li>
+              <li><i class="ion-android-checkmark-circle"></i> All time available to make your problem to ours and make it solve .</li>
+            </ul>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #about -->
+<?php include 'template/footer.php'; ?>
